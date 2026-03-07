9f36f7145702516aa97d98329767b56746c6a7aa
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2026-03-08 03:08:57 +0600

    decouled io logic

--- app/sync.py
+++ app/sync.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BLOCKSIZE = 65536
 
 
-def hash_file(path):
+def hash_file(path: Path):
     hasher = hashlib.sha1()
     with path.open("rb") as file:
         buf = file.read(BLOCKSIZE)
@@ -16,27 +16,48 @@ def hash_file(path):
     return hasher.hexdigest()
 
 
+def read_paths_and_hashes(root):
+    hashes = {}
+    for folder, _, files in os.walk(root):
+        for fn in files:
+            abspath = Path(folder) / fn
+            relpath = abspath.relative_to(root)
+            hashes[hash_file(Path(folder) / fn)] = relpath
+    return hashes
+
+
+def determine_actions(
+    source_hashes: dict, dest_hashes: dict, source_folder, dest_folder
+):
+    for sha, relpath in source_hashes.items():
+        if sha not in dest_hashes:
+            sourcepath = Path(source_folder) / relpath
+            destpath = Path(dest_folder) / relpath
+            yield "COPY", sourcepath, destpath
+
+        elif dest_hashes[sha] != relpath:
+            olddestpath = Path(dest_folder) / dest_hashes[sha]
+            newdestpath = Path(dest_folder) / relpath
+            yield "MOVE", olddestpath, newdestpath
+
+    for sha, relpath in dest_hashes.items():
+        if sha not in source_hashes:
+            yield "DELETE", dest_folder / relpath
+
+
 def sync(source: Path, dest: Path):
-    source_hashes = {}
-    for folder, _, files in os.walk(source):
-        for fn in files:
-            source_hashes[hash_file(Path(folder) / fn)] = fn
+    source_hashes = read_paths_and_hashes(source)
+    dest_hashes = read_paths_and_hashes(dest)
 
-    seen = set()
+    actions = determine_actions(source_hashes, dest_hashes, source, dest)
 
-    for folder, _, files in os.walk(dest):
-        for fn in files:
-            dest_path = Path(folder) / fn
-            dest_hash = hash_file(dest_path)
-            seen.add(dest_hash)
+    for action, *paths in actions:
+        if action == "COPY":
+            shutil.copyfile(*paths)
 
-            if dest_hash not in source_hashes:
-                # dest_path.remove()
-                dest_path.unlink()
+        elif action == "MOVE":
+            paths[1].parent.mkdir(parents=True, exist_ok=True)
+            shutil.move(*paths)
 
-            elif dest_hash in source_hashes and fn != source_hashes[dest_hash]:
-                shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
-
-    for source_hash, fn in source_hashes.items():
-        if source_hash not in seen:
-            shutil.copy(Path(source) / fn, Path(dest) / fn)
+        else:
+            paths[0].unlink()
