1940e24aa1673c62c4e56116d619100b411e9a7e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2026-04-08 14:57:55 +0600

    adding redis client

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+import os
+
+
 def get_api_url() -> str:
     return "http://localhost:8000"
 
@@ -6,3 +9,9 @@ def get_postgres_uri():
     # postgres is the default database
     # it automatically creates as the user_name
     return "postgresql://postgres:secret@localhost:5432/postgres"
+
+
+def get_redis_host_and_port():
+    host = os.environ.get("REDIS_HOST", "localhost")
+    port = 63791 if host == "localhost" else 6379
+    return {"host": host, "port": port}
